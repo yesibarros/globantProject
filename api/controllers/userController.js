d8f7dfc62a080cf6ae67c48bb9c41e76673764eb
mariaGuadalupeB <mariagballesi@gmail.com>
2021-03-24 21:15:16 -0300

    controller route

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -4,10 +4,30 @@ const {User} = require('../models/User')
 const userController = {}
 
 userController.getUser = (req, res, next) => {
-    res.send("userPerfil")
+    const { id } = req.body
+    User.findById(userId)
+    .populate("")
+    .then( userProfile => {
+        if(!userProfile) res.sendStatus(404)
+        else res.status(200).send(userProfile)
+    })
+    .catch(next)
 } 
+
+// location
+//   mentees
+//   mentor
+//   area
+//   technologies
+  
+
+
+
 userController.getAllUserbyParam =  (req, res, next) => {
+
+    
     res.send("userByParam mentee o mentor")
 }
 
 module.exports = userController;
+
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const userController = require("../controllers/userController");
+// const authJWT = require('../middleware/authJWT');
 
-router.get("/", userController.getUser);
+router.get("/", userController.getUser); // agregar midleware --> authJWT
 router.get("/userstype" , userController.getAllUserbyParam);
 
 
