2b74529171767ea7ed16b1086062034a51e87c91
yesibarros <yesibarros95@gmail.com>
2021-03-26 21:33:27 +0000

    Profile changes

--- front/screens/profile/Profile.jsx
+++ front/screens/profile/Profile.jsx
@@ -1,50 +1,45 @@
-import React,{useEffect} from "react";
-import { StyleSheet, View, Text, ScrollView} from "react-native";
-import Header from "../../shared/Header"
-import { Avatar } from 'react-native-elements';
+import React, { useEffect } from "react";
+import { StyleSheet, View, Text, ScrollView, FlatList } from "react-native";
+import Header from "../../shared/Header";
+import { Avatar } from "react-native-elements";
 import { Button } from "react-native";
-import styles from "./profileStyle"
+import styles from "./profileStyle";
 //REDUX
 import { useDispatch, useSelector } from "react-redux";
-import {login} from '../../state/loggedUser/thunks'
-import {logout} from '../../state/loggedUser/actions'
+import { login } from "../../state/loggedUser/thunks";
+import { logout } from "../../state/loggedUser/actions";
 
-
-
-export default function Profile({navigation}) {
+export default function Profile({ navigation }) {
   const dispatch = useDispatch();
-  const loginUser= useSelector((state) => state.loggedUser.user);
-  
+  const loginUser = useSelector((state) => state.loggedUser.user);
 
-  const handleSessionLogout =()=>{
-dispatch(logout())
-   navigation.navigate('SignIn')
-    
+  const handleSessionLogout = () => {
+    dispatch(logout());
+    navigation.navigate("SignIn");
+  };
 
-    
-  }
-
-
-  
-    return (
-     
-      <View >
-      {loginUser._id &&
+  return (
+    <View>
+      {loginUser._id && (
         <View style={styles.header}>
           <Header />
           <View style={styles.centerView}>
-      
             <Avatar
               size="xlarge"
               source={{
-                uri:
-                  loginUser.img,
-                width: "100%", 
-                heigth: "100%"
+                uri: loginUser.img,
+                width: "100%",
+                heigth: "100%",
               }}
               rounded
-              title={loginUser.firstName+loginUser.lastName}
-              titleStyle={{color: "white", backgroundColor: "gray", flex: 1, width: "100%", paddingTop:"15%"}}
+              title={loginUser.firstName + loginUser.lastName}
+              titleStyle={{
+                color: "white",
+                backgroundColor: "gray",
+                flex: 1,
+                width: "100%",
+                paddingTop: "15%",
+              }}
               onPress={() => console.log("Works!")}
               activeOpacity={0.7}
             />
@@ -52,34 +47,45 @@ dispatch(logout())
           <Text style={styles.userText}>
             {loginUser.firstName} {loginUser.lastName}
           </Text>
-        
-        <View style={styles.infoContainer}>
-          <Text style={styles.infoTitle}>Sede:</Text>
-          <Text style={styles.infoContent}>{loginUser.location[0].locationName}</Text>
-          {/* <Text style={styles.infoTitle}>Puesto:</Text>
-          <Text style={styles.infoContent}>{user.puesto}</Text> */}
-          <Text style={styles.infoTitle}>Rol:</Text>
-          <Text style={styles.infoContent}>{loginUser.role}</Text>
-          <Text style={styles.infoTitle}  >Habilidades</Text>
-          
-          {loginUser.technologies &&
-            loginUser.technologies.map((tech) => {
-              
-              return (
 
-                <Text style={styles.infoContent} /*key={tech.tech._id}*/  >
-                  -{tech.technologyName}
-                </Text>
-              );
-            })}
+          <View style={styles.infoContainer}>
+            <Text style={styles.infoTitle}>Sede:</Text>
+            <Text style={styles.infoContent}>
+              {loginUser.location[0].locationName}
+            </Text>
+            {/* <Text style={styles.infoTitle}>Puesto:</Text>
+            <Text style={styles.infoContent}>{user.puesto}</Text> */}
+            <Text style={styles.infoTitle}>Rol:</Text>
+            <Text style={styles.infoContent}>{loginUser.role}</Text>
+            <Text style={styles.infoTitle}>Habilidades</Text>
+         
+           
+           
+           {loginUser.technologies.length &&
+            <FlatList
+              keyExtractor={tech=> tech.technologyName}
+              data={loginUser.technologies}
+              renderItem={({ item }) => {
+                
+      
+                  return (
+                  <Text style={styles.infoContent}>{item.technologyName}</Text>
+                  )
+              
+                
+              }}
+            />
+          }
+           </View>
+            
+          <View style={styles.button}>
+            <Button
+              onPress={handleSessionLogout}
+              title={"Cerrar sesión"}
+            ></Button>
+          </View>
         </View>
-        <View style={styles.button}>
-          <Button onPress={handleSessionLogout} title={"Cerrar sesión"}></Button>
-        </View>
-</View>
+      )}
+    </View>
+  );
 }
-      </View>
-    );
-  }
-  
-  
