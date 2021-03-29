4c630c309bb2b6fc8734c0c592019b426a737a81
SantuchoSJ <santiagojoaquinsantucho@gmail.com>
2021-03-29 14:56:30 -0300

    Register funcional

--- api/config/index.js
+++ api/config/index.js
@@ -5,27 +5,27 @@ const mongoose = require("mongoose");
 const { DB_NAME, DB_PASSWORD } = process.env;
 
 //MONGO ATLAS
- mongoose
-  .connect(
-    `mongodb+srv://globant:${DB_PASSWORD}@globant.7ahj7.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    }
-  )
-  .then(() => console.log("DB CONNECT!!"))
-  .catch((e) => console.log("DB NO CONNECT ERROR", e));  
-
-//BASE LOCAL DE PRUEBA
-//  mongoose.connect("mongodb://localhost/pruebaGlobant", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useFindAndModify: false,
-//     useCreateIndex: true
-//   })
+//  mongoose
+//   .connect(
+//     `mongodb+srv://globant:${DB_PASSWORD}@globant.7ahj7.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
+//     {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useFindAndModify: false,
+//       useCreateIndex: true,
+//     }
+//   )
 //   .then(() => console.log("DB CONNECT!!"))
-//   .catch((e)=> console.log(e))
+//   .catch((e) => console.log("DB NO CONNECT ERROR", e));  
+
+// BASE LOCAL DE PRUEBA
+ mongoose.connect("mongodb://localhost/pruebaGlobant", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  })
+  .then(() => console.log("DB CONNECT!!"))
+  .catch((e)=> console.log(e))
 
 module.exports = mongoose;
--- front/state/loggedUser/thunks.js
+++ front/state/loggedUser/thunks.js
@@ -3,14 +3,14 @@ import axios from "axios"
 
 
 export const register= createAsyncThunk("REGISTER_REQUEST", (data)=>{
-    return axios.post("/api/auth/register", data).then((respuesta)=>[])
+    return axios.post("http://192.168.0.110:5000/api/auth/register", data).then((respuesta)=>respuesta.data)
   })
 
 
 export const login= createAsyncThunk("LOGIN_REQUEST", (data)=>{
   // console.log("entre al login back", data)
   return axios
-      .post("http://localhost:5000/api/auth/login", {email:data.email, password:data.password})
+      .post("http://192.168.0.110:5000/api/auth/login", {email:data.email, password:data.password})
       .then((respuesta) =>  respuesta.data)
 })
 
