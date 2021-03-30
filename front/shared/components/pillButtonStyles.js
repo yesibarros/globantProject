14a21dafeca542cff35f46379395658b9c7f2f6f
yesibarros <yesibarros95@gmail.com>
2021-03-30 18:29:52 +0100

    profile skills

---
+++ front/shared/components/pillButtonStyles.js
@@ -0,0 +1,22 @@
+import { StyleSheet} from "react-native";
+const styles = StyleSheet.create({
+    
+    textSign: {
+      fontSize: 15,
+      fontWeight: "bold",
+      alignContent: "center",
+      color: "black",
+    },
+    circular: {
+      marginTop: 15,
+    //   width: "65%",
+      marginLeft:10,  
+      padding:10,
+      justifyContent: "center",
+      alignItems: "center",
+      borderRadius: 10,
+      backgroundColor: "lightgrey",
+    },
+  });
+  
+  export default styles
