181dd251a73eef26c54e7f9cae407abb2de6618d
SantuchoSJ <santiagojoaquinsantucho@gmail.com>
2021-04-15 15:01:23 -0300

    ver perfil mentee

--- front/state/objetivos/reducer.js
+++ front/state/objetivos/reducer.js
@@ -6,7 +6,10 @@ import { getObjective } from "./actions";
 let objetives = [];
 
 const objetivosReducer = createReducer(objetives, {
-  [getObjectives.fulfilled]: (state, action) => action.payload,
+  [getObjectives.fulfilled]: (state, action) => {
+    console.log(action)
+    return action.payload
+  },
 });
 
 export default objetivosReducer;
