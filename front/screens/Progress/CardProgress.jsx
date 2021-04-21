e0539330c5fa99989279f5d0e02581b12e0ce2b1
mariaGuadalupeB <mariagballesi@gmail.com>
2021-04-21 15:27:29 -0300

    responsive objetivos en proceso

--- front/screens/Progress/CardProgress.jsx
+++ front/screens/Progress/CardProgress.jsx
@@ -23,7 +23,7 @@ import { primaryGreen } from "../../utils/Colors";
 import { useDispatch, useSelector } from "react-redux";
 import { setUser } from "../../state/loggedUser/actions";
 import { cancelRequest, acceptRequest } from "../../state/requests/Thunks";
-import { updateObjective, getObjectives } from '../../state/objetivos/thunks';
+import { updateObjective, getObjectives } from "../../state/objetivos/thunks";
 import styles from "./cardProgressStyle";
 import { ScrollView } from "react-native-gesture-handler";
 
@@ -34,193 +34,178 @@ const CardProgress = ({ item, last }) => {
   const [selected, setSelected] = useState();
   const dispatch = useDispatch();
   const border = last ? "transparent" : "lightgrey";
-  const idMentee = item.mentee && item.mentee._id 
+  const idMentee = item.mentee && item.mentee._id;
 
-  const handleUpdate = ( ) => {
-    if(!feedbackMentor && selected !== 'pending'){
-      return alert('Debes dar una devolución')
-    }else{
-      dispatch(updateObjective({
-        id: item._id,
-        status: selected,
-        feedback: feedbackMentor
-      })).then(() => dispatch(getObjectives(idMentee))).then(() => {
-        setViewModal(false)
-      })
+  const handleUpdate = () => {
+    if (!feedbackMentor && selected !== "pending") {
+      return alert("Debes dar una devolución");
+    } else {
+      dispatch(
+        updateObjective({
+          id: item._id,
+          status: selected,
+          feedback: feedbackMentor,
+        })
+      )
+        .then(() => dispatch(getObjectives(idMentee)))
+        .then(() => {
+          setViewModal(false);
+        });
     }
-  }
+  };
 
   return (
     <View style={styles.progressContainer}>
-    <View style={[styles.container, { borderColor: border }]}>
-      <Avatar.Icon
-        size={55}
-        color="#009387"
-        icon={item.status === 'pending' ? 'progress-clock' : "check-bold"}
-        style={styles.avatar}
-      ></Avatar.Icon>
-      <Card style={styles.card}>
-        <Card.Title
-          title={item.objectiveName}
-          subtitle={item.description}
-          right={(props) => (
-            <IconButton
-              {...props}
-              size={35}
-              color="#009387"
-              icon="eye-plus"
-              onPress={() => {
-                setViewModal(true);
-              }}
-            />
-          )}
-        />
-      </Card>
-
-      <KeyboardAvoidingView
-        behavior="padding"
-        style={{
-          flex: 1,
-        }}
-        enabled={Platform.OS === "android"}
-        keyboardVerticalOffset={80}
-      >
-        <Modal visible={viewModal} transparent={true} animationType="slide">
-          <View style={styles.viewContainer}>
-            <View
-              style={{
-                flex: 0.3,
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 30,
-                  fontWeight: "bold",
-                  alignContent: "center",
-                  textTransform: "uppercase",
-                  marginLeft: 10
-                }}
-              >
-                {item.objectiveName}
-              </Text>
-            </View>
-
-            <Card style={styles.empty}>
-              <ScrollView>
-                <Text
-                  style={{
-                    fontSize: 20,
-                    padding: 10,
-                  }}
-                >
-                  {item.description}
-                </Text>
-              </ScrollView>
-            </Card>
-            <View
-              style={{
-                flex: 0.7,
-                justifyContent: "space-around",
-              }}
-            >
-              <Text style={{ fontSize: 20, paddingLeft: 20 }}>
-                Fecha: 20/4/2020
-              </Text>
-                {loggedUser.role && loggedUser.role[0] === 'mentor' 
-                ?
-                <View style={{flexDirection: 'row', alignItems: "center"}}>
-                <Text style={{ fontSize: 20, paddingLeft: 20}}>
-                  Modificar Estado: 
-                </Text>
-                <SelectPicker
-                  onValueChange={(value) => {
-                    setSelected(value);
-                  }}
-                  selected={selected}
-                  style={styles.inputLocation}
-                  placeholder="Selecciona el estado"
-                >
-                <SelectPicker.Item
-                  label='Pendiente'
-                  value='pending'
-                  key='pending'
-                />
-                <SelectPicker.Item
-                  label='Logrado'
-                  value='achieved'
-                  key='achieved'
-                />
-                <SelectPicker.Item
-                  label='Excelente'
-                  value='excellent'
-                  key='excellent'
-                  />
-                </SelectPicker>
-                </View>
-
-                : 
-              <Text style={{ fontSize: 20, paddingLeft: 20 }}>
-                  Estado: {item.status}
-              </Text>
-                }
-              {loggedUser && loggedUser.role && loggedUser.role.includes("mentor") ? (
-                <View
-                  style={{
-                    alignItems: "center",
-                  }}
-                >
-                  <TextInput
-                    style={{ width: "90%" }}
-                    label="Devolucion"
-                    value={feedbackMentor}
-                    onChangeText={(feedbackMentor) =>
-                      setFeedbackMentor(feedbackMentor)
-                    }
-                  />
-                </View>
-              ) : (
-                <Text style={{ fontSize: 20, paddingLeft: 20 }}>
-                  Devolucion:
-                  {item.feedback
-                    ? item.feedback
-                    : "No hay devoluciones todavia."}
-                </Text>
-              )}
-            </View>
-            {/* crear un select que tenga 3 opciones */}
-            {/* implementar calender */}
-            <TouchableOpacity />
-
-            <View
-              style={{
-                flex: 0.4,
-                alignItems: "center",
-                flexDirection: "row",
-                justifyContent: "space-around",
-              }}
-            >
-              {/* <View > */}
-              <Button
-                style={{
-                  backgroundColor: "#009387",
-                  width: "40%",
-                  height: "40%",
-                  justifyContent: "center",
-                }}
+      <View style={[styles.container, { borderColor: border }]}>
+        <Avatar.Icon
+          size={55}
+          color="#009387"
+          icon={item.status === "pending" ? "progress-clock" : "check-bold"}
+          style={styles.avatar}
+        ></Avatar.Icon>
+        <Card style={styles.card}>
+          <Card.Title
+            title={item.objectiveName}
+            subtitle={item.description}
+            right={(props) => (
+              <IconButton
+                {...props}
+                size={35}
+                color="#009387"
+                icon="eye-plus"
                 onPress={() => {
-                  setViewModal(false);
+                  setViewModal(true);
+                }}
+              />
+            )}
+          />
+        </Card>
+
+        <KeyboardAvoidingView
+          behavior="padding"
+          style={{
+            flex: 1,
+          }}
+          enabled={Platform.OS === "android"}
+          keyboardVerticalOffset={80}
+        >
+          <Modal visible={viewModal} transparent={true} animationType="slide">
+            <View style={styles.viewContainer}>
+              <View
+                style={{
+                  flex: 0.3,
+                  justifyContent: "center",
+                  alignItems: "center",
                 }}
               >
                 <Text
-                  style={{ fontSize: 22, color: "white", textAlign: "center" }}
+                  style={{
+                    fontSize: 30,
+                    fontWeight: "bold",
+                    alignContent: "center",
+                    textTransform: "uppercase",
+                    marginLeft: 10,
+                  }}
                 >
-                  Cerrar
+                  {item.objectiveName}
                 </Text>
-              </Button>
+              </View>
 
-              {loggedUser && loggedUser.role && loggedUser.role.includes("mentor") ? (
+              <Card style={styles.empty}>
+                <ScrollView>
+                  <Text
+                    style={{
+                      fontSize: 20,
+                      padding: 10,
+                    }}
+                  >
+                    {item.description}
+                  </Text>
+                </ScrollView>
+              </Card>
+              <View
+                style={{
+                  flex: 0.7,
+                  justifyContent: "space-around",
+                }}
+              >
+                <Text style={{ fontSize: 20, paddingLeft: 20 }}>
+                  Fecha: 20/4/2020
+                </Text>
+                {loggedUser.role && loggedUser.role[0] === "mentor" ? (
+                  <View style={{ flexDirection: "row", alignItems: "center" }}>
+                    <Text style={{ fontSize: 20, paddingLeft: 20 }}>
+                      Modificar Estado:
+                    </Text>
+                    <SelectPicker
+                      onValueChange={(value) => {
+                        setSelected(value);
+                      }}
+                      selected={selected}
+                      style={styles.inputLocation}
+                      placeholder="Selecciona el estado"
+                    >
+                      <SelectPicker.Item
+                        label="Pendiente"
+                        value="pending"
+                        key="pending"
+                      />
+                      <SelectPicker.Item
+                        label="Logrado"
+                        value="achieved"
+                        key="achieved"
+                      />
+                      <SelectPicker.Item
+                        label="Excelente"
+                        value="excellent"
+                        key="excellent"
+                      />
+                    </SelectPicker>
+                  </View>
+                ) : (
+                  <Text style={{ fontSize: 20, paddingLeft: 20 }}>
+                    Estado: {item.status}
+                  </Text>
+                )}
+                {loggedUser &&
+                loggedUser.role &&
+                loggedUser.role.includes("mentor") ? (
+                  <View
+                    style={{
+                      alignItems: "center",
+                    }}
+                  >
+                    <TextInput
+                      style={{ width: "90%" }}
+                      label="Devolucion"
+                      value={feedbackMentor}
+                      onChangeText={(feedbackMentor) =>
+                        setFeedbackMentor(feedbackMentor)
+                      }
+                    />
+                  </View>
+                ) : (
+                  <Text style={{ fontSize: 20, paddingLeft: 20 }}>
+                    Devolucion:
+                    {item.feedback
+                      ? item.feedback
+                      : "No hay devoluciones todavia."}
+                  </Text>
+                )}
+              </View>
+              {/* crear un select que tenga 3 opciones */}
+              {/* implementar calender */}
+              <TouchableOpacity />
+
+              <View
+                style={{
+                  flex: 0.4,
+                  alignItems: "center",
+                  flexDirection: "row",
+                  justifyContent: "space-around",
+                }}
+              >
+                {/* <View > */}
                 <Button
                   style={{
                     backgroundColor: "#009387",
@@ -229,7 +214,7 @@ const CardProgress = ({ item, last }) => {
                     justifyContent: "center",
                   }}
                   onPress={() => {
-                    return handleUpdate()
+                    setViewModal(false);
                   }}
                 >
                   <Text
@@ -239,15 +224,40 @@ const CardProgress = ({ item, last }) => {
                       textAlign: "center",
                     }}
                   >
-                    GUARDAR
+                    Cerrar
                   </Text>
                 </Button>
-              ) : null}
+
+                {loggedUser &&
+                loggedUser.role &&
+                loggedUser.role.includes("mentor") ? (
+                  <Button
+                    style={{
+                      backgroundColor: "#009387",
+                      width: "40%",
+                      height: "40%",
+                      justifyContent: "center",
+                    }}
+                    onPress={() => {
+                      return handleUpdate();
+                    }}
+                  >
+                    <Text
+                      style={{
+                        fontSize: 22,
+                        color: "white",
+                        textAlign: "center",
+                      }}
+                    >
+                      GUARDAR
+                    </Text>
+                  </Button>
+                ) : null}
+              </View>
             </View>
-          </View>
-        </Modal>
-      </KeyboardAvoidingView>
-    </View>
+          </Modal>
+        </KeyboardAvoidingView>
+      </View>
     </View>
   );
 };
--- front/screens/Progress/Progress.jsx
+++ front/screens/Progress/Progress.jsx
@@ -1,31 +1,47 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, View, Text, ScrollView, FlatList, Modal, TextInput, Dimensions,KeyboardAvoidingView, TouchableOpacity} from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  ScrollView,
+  FlatList,
+  Modal,
+  TextInput,
+  Dimensions,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+} from "react-native";
 import { Title, IconButton, Card, Button } from "react-native-paper";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 import { useTheme } from "@react-navigation/native";
-import {state} from "../../utils/state"
+import { state } from "../../utils/state";
 import TabBar from "../../routes/Tab/TabBar";
 import styles from "./progressStyle";
 import CardProgress from "./CardProgress";
 import { useDispatch, useSelector } from "react-redux";
 import { getObjectives, sendObjective } from "../../state/objetivos/thunks";
+import Header from "../header/Header";
 
 export default function Progress({ route, navigation }) {
   const [viewModal, setViewModal] = useState(false);
-  const idCurrent = route && route.params && route.params.idCurrent
+  const idCurrent = route && route.params && route.params.idCurrent;
   const { colors } = useTheme();
   const { height } = Dimensions.get("window");
   const dispatch = useDispatch();
   const logginUser = useSelector((state) => state.loggedUser.user);
   const goals = useSelector((state) => state.objetivos);
   const id = idCurrent || logginUser._id;
-  const [objective, setObjective] = useState("")
-  const [titleObjective, setTitleObjective] = useState("")
+  const [objective, setObjective] = useState("");
+  const [titleObjective, setTitleObjective] = useState("");
 
   useEffect(() => {
     // if(logginUser.role && logginUser.role[0] === 'mentor'){
     //   navigation.navigate('Mis mentees')
     // }
-  
+
     dispatch(getObjectives(id));
   }, [id]);
 
@@ -35,74 +51,102 @@ export default function Progress({ route, navigation }) {
       description: objective,
       mentor: logginUser._id,
       mentee: idCurrent,
-    }
-    dispatch(sendObjective(obj)).then((data) => 
+    };
+    dispatch(sendObjective(obj)).then((data) =>
       dispatch(getObjectives(idCurrent))
-    )
-  }
+    );
+  };
 
   return (
-   <View style={{flex:1}}>
-     <IconButton
-                    icon="menu"
-                    color={colors.icon}
-                    size={35}
-                    style={{position: "absolute", marginTop:30, zIndex:1}}
-                    onPress={() => navigation.openDrawer()}
-                  />
-     {console.log("CURRENT ID", idCurrent)}
-    <View style={{height: "100%", paddingBottom: 55, maxHeight: height}}>
-      <View style={{flexDirection: 'row', justifyContent: 'center',alignItems: 'center'}}>
-        <Text style={styles.titleProgress}>Objetivos</Text>
-      {logginUser.role && logginUser.role[0] === 'mentor'
-      ?
-      <IconButton
-        size={35}
-        color="#009387"
-        icon="plus"
-        onPress={() => {
-          setViewModal(true)
-        }}
-      />
-      :
-      null
-      }
-      </View>
+    <View style={{ flex: 1, backgroundColor: "#009387" }}>
+      <Header navigation={navigation} />
 
-        <Modal visible={viewModal} transparent={true} animationType="slide">
-        <View style={styles.viewContainer}>
+      <View
+        style={{
+          height: hp("100%"),
+          borderTopLeftRadius: 60,
+          backgroundColor: "white",
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            height: hp("11%"),
+          }}
+        >
+          <Text style={styles.titleProgress}>Objetivos</Text>
+          {/* logginUser.role && logginUser.role[0] === "mentor" */}
+          {true ? (
             <View
               style={{
-                flex: 0.3,
+                position: "absolute",
+                width: wp("95%"),
+              }}
+            >
+              <IconButton
+                size={hp("5%")}
+                color="#009387"
+                icon="plus"
+                style={{ alignSelf: "flex-end" }}
+                onPress={() => {
+                  setViewModal(true);
+                }}
+              />
+            </View>
+          ) : null}
+        </View>
+
+        <Modal visible={viewModal} transparent={true} animationType="slide">
+          <View style={styles.viewContainer}>
+            <View
+              style={{
+                height: hp("40%"),
                 justifyContent: "center",
                 alignItems: "center",
               }}
             >
-              <Text>Titulo del objetivo:</Text>
+              <Text style={styles.modalInstruction}>Titulo del objetivo:</Text>
               <TextInput
-                style={[styles.input, {color: "#858585", fontSize: 15, backgroundColor: "rgba(255, 255, 255, 0.7)"}]}
+                style={[
+                  styles.input,
+                  {
+                    color: "#858585",
+                    fontSize: hp("3%"),
+                    backgroundColor: "rgba(255, 255, 255, 0.7)",
+                  },
+                ]}
                 multiline
                 value={titleObjective}
-                onChangeText={text => setTitleObjective(text)}
+                onChangeText={(text) => setTitleObjective(text)}
               />
-              <Text>Descripción del objetivo:</Text>
+              <Text style={styles.modalInstruction}>
+                Descripción del objetivo:
+              </Text>
               <TextInput
-                style={[styles.input, {color: "#858585", fontSize: 15, backgroundColor: "rgba(255, 255, 255, 0.7)"}]}
+                style={[
+                  styles.input,
+                  {
+                    color: "#858585",
+                    fontSize: hp("3%"),
+                    backgroundColor: "rgba(255, 255, 255, 0.7)",
+                  },
+                ]}
                 multiline
                 value={objective}
-                onChangeText={text => setObjective(text)}
+                onChangeText={(text) => setObjective(text)}
               />
             </View>
 
             <View
               style={{
-                flex: 0.4,
+                height: hp("12%"),
                 alignItems: "center",
                 flexDirection: "row",
                 justifyContent: "space-around",
               }}
             >
-            
               <Button
                 style={styles.Button}
                 onPress={() => {
@@ -110,56 +154,58 @@ export default function Progress({ route, navigation }) {
                 }}
               >
                 <Text
-                  style={{ fontSize: 22, color: "white", textAlign: "center" }}
+                  style={{
+                    fontSize: hp("3%"),
+                    color: "white",
+                    textAlign: "center",
+                  }}
                 >
                   Cerrar
                 </Text>
               </Button>
-                
+
               <Button
-                  style={styles.Button}
-                  onPress={() => {
-                    setViewModal(false);
-                    return handleObjective()
+                style={styles.Button}
+                onPress={() => {
+                  setViewModal(false);
+                  return handleObjective();
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: hp("3%"),
+                    color: "white",
+                    textAlign: "center",
                   }}
                 >
-                  <Text
-                    style={{
-                      fontSize: 22,
-                      color: "white",
-                      textAlign: "center",
-                    }}
-                  >
-                    ENVIAR
-                  </Text>
-                </Button>
+                  Enviar
+                </Text>
+              </Button>
             </View>
-        </View>
+          </View>
         </Modal>
-      <View style={{flex: 1,}}>
-        {goals && goals.length > 0 
-        ? 
-        <FlatList
-          data={goals}
-          keyExtractor={item => item._id}
-          renderItem={(goal) => {
-            const last = goal.item._id === goals[goals.length - 1]._id ? true : false;
-            return (
-                <CardProgress item={goal.item} last={last} />
-            )
-          }}
-        />
-        :
-        <View style={styles.n}>
-          <Text style={styles.nText}>No tienes objetivos establecidos</Text>
+
+        <View style={{}}>
+          {goals && goals.length > 0 ? (
+            <FlatList
+              data={goals}
+              keyExtractor={(item) => item._id}
+              renderItem={(goal) => {
+                const last =
+                  goal.item._id === goals[goals.length - 1]._id ? true : false;
+                return <CardProgress item={goal.item} last={last} />;
+              }}
+            />
+          ) : (
+            <View style={styles.n}>
+              <Text style={styles.nText}>No tienes objetivos establecidos</Text>
+            </View>
+          )}
         </View>
-        }
       </View>
-    </View>
       <View style={{}}>
         <TabBar state={state} navigation={navigation} />
       </View>
     </View>
-  
   );
 }
