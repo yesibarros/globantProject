2fc1cf34c4eb0acc9ea666fd30a2ee06d401de1a
SantuchoSJ <santiagojoaquinsantucho@gmail.com>
2021-04-22 12:18:02 -0300

    single user parcial

--- api/config/index.js
+++ api/config/index.js
@@ -6,25 +6,25 @@ const { networkInterfaces } = require("os");
 const { DB_NAME, DB_PASSWORD } = process.env;
 
 // MONGO ATLAS
-mongoose
-  .connect(
-    `mongodb+srv://globant:${DB_PASSWORD}@globant.7ahj7.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-      useCreateIndex: true,
-    }
-  )
-  .then(() => console.log("DB CONNECT!!"))
-  .catch((e) => console.log("DB NO CONNECT ERROR", e));
+// mongoose
+//   .connect(
+//     `mongodb+srv://globant:${DB_PASSWORD}@globant.7ahj7.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
+//     {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//       useFindAndModify: false,
+//       useCreateIndex: true,
+//     }
+//   )
+//   .then(() => console.log("DB CONNECT!!"))
+//   .catch((e) => console.log("DB NO CONNECT ERROR", e));
 
 //BASE LOCAL DE PRUEBA
-// mongoose.connect(`mongodb://localhost/globant`, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useFindAndModify: false,
-//   useCreateIndex: true
-// }).then(()=> console.log("Connected to the local db"));
+mongoose.connect(`mongodb://localhost/globant`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}).then(()=> console.log("Connected to the local db"));
 
 module.exports = mongoose;
